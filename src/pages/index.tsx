5fd5358092c85c44b3994e54e068509509b83a93
LukaPatarcic <luka.patarcic@auxality.com>
2022-07-16 13:09:48 +0200

    added design for main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,75 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { trpc } from "../utils/trpc";
+import type {NextPage} from "next";
+import ReactTooltip from 'react-tooltip';
+import {trpc} from "../utils/trpc";
 import {FormEvent, useState} from "react";
+import {z} from "zod";
 
+const schema = z.string().url();
 const Home: NextPage = () => {
-    const [slug, setSlug] = useState('');
-  const { mutateAsync, isLoading } = trpc.useMutation(["shorten"]);
+	const [slug, setSlug] = useState('');
+	const [url, setUrl] = useState('');
+	const [error, setError] = useState('');
+	const [showTooltip, setShowTooltip] = useState(false);
+	const {mutateAsync, isLoading} = trpc.useMutation(["shorten"]);
 
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      const form = new FormData(e.currentTarget)
-      const url = form.get('url')?.toString();
-      if(!url) return;
-      mutateAsync({ url }).then((slug) => {
-          setSlug(slug);
-      }).catch((err) => {
-          console.log(err.message);
-      });
-  }
+	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const result = schema.safeParse(url);
+		if (!result.success) return;
+		mutateAsync({url}).then((slug) => {
+			setSlug(slug);
+		}).catch((err) => {
+			console.log(err.message);
+		});
+	}
 
-  return (
-    <div className="">
-      <form onSubmit={onSubmit}>
-        <input name="url" />
-        <button type="submit">{isLoading ? 'Loading...' : 'Submit'}</button>
-      </form>
-        <div>
-            {slug ? `${window.location.host}/${slug}` : ''}
-        </div>
-    </div>
-  );
+	const onChange = (value: string) => {
+		const result = schema.safeParse(value);
+		if (!result.success) {
+			setError(result.error.message);
+		} else {
+			setError('');
+		}
+
+		setUrl(value);
+	}
+
+	const onCopy = () => {
+		navigator.clipboard.writeText(`${window.location.origin}/${slug}`);
+		setShowTooltip(true);
+		const timeout = setTimeout(() => {
+			setShowTooltip(false)
+		}, 1000)
+
+		if(showTooltip) {
+			clearTimeout(timeout);
+		}
+	}
+
+	return (
+		<>
+			<div className="flex justify-center items-center h-screen">
+				<form onSubmit={onSubmit} className="w-full max-w-sm">
+					<h1 className="mb-3 text-3xl font-bold">Copy a link and shorten it</h1>
+					<input
+						className="w-full mb-4 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+						id="inline-full-name" type="text" name="url" value={url}
+						placeholder="Paste your link here..."
+						onChange={(e) => onChange(e.target.value)}/>
+					{slug &&
+						<>
+						<div className="mb-2 underline text-center cursor-pointer" onClick={onCopy}>
+							{slug ? `${window.location.origin}/${slug}` : ''}
+						</div>
+						{showTooltip && <div className="text-green-600 text-center">Copied</div>}
+                        </>}
+					<button disabled={!!error || isLoading}
+							className="disabled:opacity-75 disabled:hover:bg-blue-500 disabled:cursor-not-allowed w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+							type="submit">{isLoading ? 'Loading...' : 'Shorten'}</button>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default Home;
--- src/server/router/urlShorten.ts
+++ src/server/router/urlShorten.ts
@@ -2,6 +2,7 @@ import { createRouter } from "./context";
 import { z } from "zod";
 import { createHash } from 'crypto';
 import { prisma} from "../db/client";
+import normalizeUrl from 'normalize-url';
 
 export const urlShortenRouter = createRouter()
   .mutation("shorten", {
@@ -10,13 +11,22 @@ export const urlShortenRouter = createRouter()
         url: z.string().url(),
       }),
     async resolve({ input, ctx }) {
-        const hash = createHash('sha256').update(input.url).digest('hex').slice(0,16);
-        await prisma.shortenUrl.create({
-             data: {
-                 url: input.url,
+        const normalizedUrl = normalizeUrl(input.url);
+        const hash = createHash('sha256').update(normalizedUrl).digest('hex').slice(0,16);
+        const urlExists = await prisma.shortenUrl.findUnique({
+            where: {
                  short_url: hash
-             }
+            }
         });
+        if(!urlExists) {
+            await prisma.shortenUrl.create({
+                data: {
+                    url: normalizedUrl,
+                    short_url: hash
+                }
+            });
+        }
+
         return hash;
     },
   });
