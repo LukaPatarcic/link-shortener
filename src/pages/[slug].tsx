eb5f6714ca2426c0bd0e1f9b7bc8dae5059f5e42
LukaPatarcic <luka.patarcic@auxality.com>
2022-07-16 10:16:41 +0200

    added redirecting and showing link

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,28 @@
+import {GetServerSideProps} from "next";
+import { prisma } from '../server/db/client'
+const Slug = () => {
+	return null;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	const shortenUrl = await prisma.shortenUrl.findFirst({
+		where: {
+			short_url: query?.slug?.toString()
+		}
+	})
+
+	if(!shortenUrl) return {
+		notFound: true,
+		props: {}
+	}
+
+	return {
+		redirect: {
+			permanent: true,
+			destination: shortenUrl.url
+		},
+		props: {}
+	}
+}
+
+export default Slug;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
-import {FormEvent} from "react";
+import {FormEvent, useState} from "react";
 
 const Home: NextPage = () => {
+    const [slug, setSlug] = useState('');
   const { mutateAsync, isLoading } = trpc.useMutation(["shorten"]);
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -11,17 +12,22 @@ const Home: NextPage = () => {
       const form = new FormData(e.currentTarget)
       const url = form.get('url')?.toString();
       if(!url) return;
-      mutateAsync({ url }).then(() => {}).catch((err) => {
+      mutateAsync({ url }).then((slug) => {
+          setSlug(slug);
+      }).catch((err) => {
           console.log(err.message);
       });
   }
 
   return (
-    <div>
+    <div className="">
       <form onSubmit={onSubmit}>
         <input name="url" />
         <button type="submit">{isLoading ? 'Loading...' : 'Submit'}</button>
       </form>
+        <div>
+            {slug ? `${window.location.host}/${slug}` : ''}
+        </div>
     </div>
   );
 };
